4d64ed2a3e3d3cd331dff099ca013732803dad46
filipl41 <filipl41@yahoo.com>
2021-09-05 03:44:10 -0700

    Feature/diff (#3)
    * diff feature
    
    * Readme updated
    
    * show non encoded
    
    * bug fixed
    
    * deo 5. poglavlja
    
    * zakljucak
    
    Co-authored-by: filip.lazic <filip.lazic@everseen.com>

--- code/main.py
+++ code/main.py
@@ -5,7 +5,8 @@ import subprocess
 import tempfile
 import tkinter as tk
 import re
-
+import difflib
+from datetime import datetime, timezone
 
 COLORS = ['alice blue', 'lavender', 'slate gray',
     'pale turquoise', 'turquoise',
@@ -39,6 +40,7 @@ class ColorLineConnect:
 color_line_map = ColorLineConnect()
 llvm_debug_line_map = {}
 next_source_file_index = 0
+encoded_real_instruction_map = {}
 
 class SourceFileConnect:
     def __init__(self, name) :
@@ -50,8 +52,11 @@ class SourceFileConnect:
             self.map[key].append(value)
         else: 
             self.map[key] = [value]
-            #color_index = color_index if color_index >= len(COLORS) else 0
-        
+
+def file_mtime(path):
+    t = datetime.fromtimestamp(os.stat(path).st_mtime,
+                               timezone.utc)
+    return t.astimezone().isoformat()        
 
 def compile_lto(source_files, optimization_level):
     tmp_folder = tempfile.mkdtemp()
@@ -93,21 +98,51 @@ def connect_source_llvm(llvm_ir_output):
         for llvm_line, source_line  in matches:
             source_llvm_map[key].insert(source_line, llvm_line)
             color_line_map.insert(source_line)
-
+    
     for values in source_llvm_map.values():
         new_values = []
         for scope_values in values.map.values():
             for scope_value in  scope_values:
-                matches = re.findall("(\![0-9]+).*line: ([0-9]+).*scope: {scope}.*".format(scope=scope_value), llvm_ir_output)
+                matches = re.findall("(\![0-9]+).*line: ([0-9]+).*scope: {scope}(?![0-9]).*".format(scope=scope_value), llvm_ir_output)
                 for llvm_line, source_line in matches:
                     new_values.append((source_line, llvm_line))
         for source_line, llvm_line in new_values:
             values.insert(source_line, llvm_line)
             color_line_map.insert(source_line)
         
-       
-
     return source_llvm_map.values()
+
+def generate_diff_string(llvm_ir, connected_files):
+    result = ""
+    string_list = llvm_ir.splitlines()
+    string_list = string_list[2:]
+    for line in string_list:
+        is_found = False
+        for connected_file in connected_files :
+            for key  in connected_file.map.keys():
+                for value in connected_file.map[key]:                
+                    match = re.search(".*\!dbg {value}(?![0-9]).*".format(value=value), line)
+                    if match:
+                        curr_line = re.sub("\!dbg {value}(?![0-9]).*".format(value=value), "", line)
+                        curr_line = re.sub("%[0-9]+ = ", "", curr_line)
+                        if curr_line[-2] == ",":
+                            curr_line = curr_line[:-2]
+                        result += connected_file.source_file_name + ":" + key + ":" + curr_line.lstrip()
+                        is_found = True
+                        encoded_real_instruction_map[connected_file.source_file_name + ":" + key + ":" + curr_line.lstrip()] = line
+                        
+                        
+        if not is_found:
+            result += line
+        result += "\n"       
+    
+    result = re.sub(";.*\n.*@llvm\.dbg\..*\n", "", result, re.MULTILINE)
+    result = re.sub("; Function.*\n", "", result)
+ 
+    result = re.sub(".*@llvm\.dbg\..*\n", "", result, re.MULTILINE)
+    result = re.sub("attributes #[0-9]+ = .*", "", result, re.MULTILINE)
+    return result
+    
 
 def parse_and_highlight_llvm(string_code, text_widget, connected_files, curr_source_file):
     for file in connected_files:
@@ -120,7 +155,7 @@ def parse_and_highlight_llvm(string_code, text_widget, connected_files, curr_sou
     for code_line in string_list:
         for key, value_list in curr_connected_file.map.items():
                 for value in value_list:
-                    match = re.search(".*\!dbg {dbg_num}.*".format(dbg_num=value), code_line)
+                    match = re.search(".*\!dbg {dbg_num}(?![0-9]).*".format(dbg_num=value), code_line)
                     if match:
                         debug = re.search(".*@llvm\.dbg\..*", code_line)
                         if debug:
@@ -187,13 +222,43 @@ def insert_text(non_lto_text_widget, string_non_lto, lto_text_widget, string_lto
     lto_text_widget.configure(state=tk.DISABLED)
     source_text_widget.configure(state=tk.DISABLED)
 
-def show_files(string_lto, string_non_lto, connected_files, source_files):
+def show_diff(lto_diff_file, non_lto_diff_file):
+    file_diff, filename_diff = tempfile.mkstemp()
+    temp = tempfile.NamedTemporaryFile()
+
+    lto_diff_file.seek(0)
+    lto_diff_string = lto_diff_file.read()
+    non_lto_diff_file.seek(0)
+    non_lto_diff_string = non_lto_diff_file.read()
+
+    lto_diff_string = lto_diff_string.decode("utf-8")
+    non_lto_diff_string = non_lto_diff_string.decode("utf-8")
+
+    todate = file_mtime(lto_diff_file.name)
+    fromdate = file_mtime(non_lto_diff_file.name)
+    
+
+    for line in difflib.unified_diff(non_lto_diff_string.splitlines(keepends=True),lto_diff_string.splitlines(keepends=True), 
+                    fromfiledate=fromdate, tofiledate=todate, fromfile=non_lto_diff_file.name, tofile=lto_diff_file.name):
+        if (line[0] == "+" or line[0] == "-") and (line[1] != "-" and line[1]!="+"):
+            if line[1:-1] in encoded_real_instruction_map.keys():
+                real_line = encoded_real_instruction_map[line[1:-1]] 
+                line = line[0] + real_line + line[-1]
+                
+        temp.write(line.encode())
+
+    temp.seek(0)
+    #print(temp.read().decode("utf-8"))
+    subprocess.check_output("kompare {out}".format(out=temp.name), shell=True)
+
+def show_files(string_lto, string_non_lto, connected_files, source_files, lto_diff_file, non_lto_diff_file):
     root = tk.Tk()
     root.geometry("1800x700+200+150")
     non_lto_text = tk.Text(root, font=("times new roman",12))
     lto_text = tk.Text(root, font=("times new roman",12))
     source_file_text = tk.Text(root, font=("times new roman",12))
-    button = tk.Button(root, text="Next source file", height=10, command=lambda: insert_text(non_lto_text, string_non_lto, lto_text, string_lto, connected_files, source_files, source_file_text, label))
+    button = tk.Button(root, text="Next source file", width=20, height=3, command=lambda: insert_text(non_lto_text, string_non_lto, lto_text, string_lto, connected_files, source_files, source_file_text, label))
+    button_show_diff = tk.Button(root, height=3, width=20, text = "Show diff Non LTO - LTO", command=lambda: show_diff(lto_diff_file, non_lto_diff_file) )
     label = tk.Label(root, anchor="w")
 
     insert_text(non_lto_text, string_non_lto, lto_text, string_lto, connected_files, source_files, source_file_text, label)
@@ -208,12 +273,13 @@ def show_files(string_lto, string_non_lto, connected_files, source_files):
     source_file_text.pack(fill=tk.BOTH, expand=True)
     source_file_text.configure(state=tk.DISABLED)
     button.pack()
+    button_show_diff.pack()
     tk.mainloop()
 
 def trim_debug_info(llvm_debug_otput):
     result = re.sub("^\![0-9]+ = .*\n", "", llvm_debug_otput, flags=re.MULTILINE)
     result = re.sub("^\!llvm\..*\n", "", result, flags=re.MULTILINE )
-    return result
+    return result.rstrip()
 
 def compile_files(folder_path, optimization_level):
     source_files = ""
@@ -227,7 +293,15 @@ def compile_files(folder_path, optimization_level):
     connected_files_non_lto = connect_source_llvm(string_non_lto)
     connected_files_lto = connect_source_llvm(string_lto)
     source_files_list = source_files.split()
-    show_files(trim_debug_info(string_lto),trim_debug_info(string_non_lto), (connected_files_lto,connected_files_non_lto), source_files_list)
+    diff_file_non_lto = trim_debug_info(generate_diff_string(string_non_lto, connected_files_non_lto))
+    diff_file_lto = trim_debug_info(generate_diff_string(string_lto, connected_files_lto))
+
+    file_lto = tempfile.NamedTemporaryFile()
+    file_non_lto = tempfile.NamedTemporaryFile()
+    file_lto.write(diff_file_lto.encode())
+    file_non_lto.write(diff_file_non_lto.encode())
+
+    show_files(trim_debug_info(string_lto),trim_debug_info(string_non_lto), (connected_files_lto,connected_files_non_lto), source_files_list, file_lto, file_non_lto)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
